714fba5cd48abbe8604c694e0544a8f34f43022f
MishaDreamer-web <shondayn@gmail.com>
2021-08-27 20:27:55 +0300

    Add axios library to API service

--- src/services/movie-api.js
+++ src/services/movie-api.js
@@ -1,39 +1,41 @@
-const API_KEY = 'ded12b962797b74c61a2522ada6bc31b';
-const BASE_URL = 'https://api.themoviedb.org/3';
+import axios from 'axios';
 
-async function fetchWithErrorHandling(url = '', config = {}) {
-  const response = await fetch(url, config);
-  return response.ok
-    ? await response.json()
-    : Promise.reject(new Error('Not found'));
-}
+const API_KEY = 'ded12b962797b74c61a2522ada6bc31b';
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+
+const fetchWithErrorHandling = async (url = '') => {
+  try {
+    const response = await axios.get(url, {
+      params: {
+        api_key: API_KEY,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    return error.response;
+  }
+};
 
 export function fetchFilms() {
-  return fetchWithErrorHandling(
-    `${BASE_URL}/trending/movie/day?api_key=${API_KEY}`,
-  );
+  return fetchWithErrorHandling(`/trending/movie/day`);
 }
 
 export function fetchFilmDetails(movieId) {
-  return fetchWithErrorHandling(
-    `${BASE_URL}/movie/${movieId}?api_key=${API_KEY}&language=en-US`,
-  );
+  return fetchWithErrorHandling(`/movie/${movieId}?language=en-US`);
 }
 
 export function fetchFilmCredits(movieId) {
-  return fetchWithErrorHandling(
-    `${BASE_URL}/movie/${movieId}/credits?api_key=${API_KEY}&language=en-US`,
-  );
+  return fetchWithErrorHandling(`/movie/${movieId}/credits?language=en-US`);
 }
 
 export function fetchFilmReviews(movieId) {
   return fetchWithErrorHandling(
-    `${BASE_URL}/movie/${movieId}/reviews?api_key=${API_KEY}&language=en-US&page=1`,
+    `/movie/${movieId}/reviews?language=en-US&page=1`,
   );
 }
 
 export function fetchMoviesByQuery(query) {
   return fetchWithErrorHandling(
-    `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${query}`,
+    `/search/movie?language=en-US&page=1&include_adult=false&query=${query}`,
   );
 }
